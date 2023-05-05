61db37103b75371db0633319c2815915a80100e5
aleokdev <aleok.inf@gmail.com>
2023-05-05 11:55:11 +0200

    Improve xOf display

--- src/components/xOf.vue
+++ src/components/xOf.vue
@@ -1,40 +1,46 @@
 <template>
-    <div style="background: rgba(238,238,238,0.25); margin: 5px 0;">
-        <div style="background: rgb(238, 238, 238); padding:4px; font-weight:bold">{{type.substring(0, 3) + ' of'}} these rules must be conformed:</div>
-        <div class="inner" v-for="(schema, idx) in value" :key="idx">
-           <json-schema :value="schema" :level="level" :max-level="maxLevel" />
-        </div>
+  <div style="background: rgba(238,238,238,0.25); margin: 5px 0;">
+    <div style="background: rgb(238, 238, 238); padding:4px; font-weight:bold">{{ type.substring(0, 3) + ' of' }} these
+      rules must be conformed:</div>
+    <div class="inner" v-for="(schema, idx) in value" :key="idx">
+      <div class="property">
+        <div style="width:2em;" />
+        <json-schema :value="schema" :level="level" :max-level="maxLevel" />
+      </div>
+      <div v-if="idx != value.length - 1" style="background: rgb(238, 238, 238); padding:4px; font-weight:bold">{{ (type
+        == "allOf") ? "and" : "or" }}</div>
     </div>
+  </div>
 </template>
 
 <script>
-  import JsonSchema from './JsonSchema'
+import JsonSchema from './JsonSchema'
 
-  export default {
-    name: 'xOf',
-    components: {
-      JsonSchema
-    },
-    props: {
-      type: String,
-      value: Array,
-      level: Number,
-      maxLevel: Number,
-    }
+export default {
+  name: 'xOf',
+  components: {
+    JsonSchema
+  },
+  props: {
+    type: String,
+    value: Array,
+    level: Number,
+    maxLevel: Number,
   }
+}
 </script>
 
 <style lang="scss">
-    .required-tag {
-        &:after {
-            color:black;
-            content: ' and ';
-        }
+.required-tag {
+  &:after {
+    color: black;
+    content: ' and ';
+  }
 
-        &:last-child {
-            &:after {
-                content: ''
-            }
-        }
+  &:last-child {
+    &:after {
+      content: ''
     }
+  }
+}
 </style>
